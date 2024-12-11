50326967191746da9be4e7bc909ef1bd4b253a7f
Bartosz Antoniak <bartosz.antoniak1@gmail.com>
2024-12-11 22:29:58 +0100

    day-29 Added: 1) saving data to text file 2) generating random strong password

--- day-29/main.py
+++ day-29/main.py
@@ -1,4 +1,45 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    if user_input.get() == "" or password_input.get() == "" or website_input.get() == "":
+        messagebox.showinfo(title="Wrong input",message="Please input data in all 3 rows")
+
+    else:
+        is_ok = messagebox.askokcancel(title=website_input.get(), message="Are details you entered ok to save?")
+        if is_ok:
+            data = open("data.txt","a")
+            data.write(f"{website_input.get()} | {user_input.get()} | {password_input.get()}\n")
+            data.close()
+            website_input.delete(0, 'end')
+            user_input.delete(0,"end")
+            password_input.delete(0,"end")
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_input.delete(0, "end")
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(0,nr_letters)]
+    password_numbers = [random.choice(numbers) for _ in range(0,nr_numbers)]
+    password_symbols = [random.choice(symbols) for _ in range(0,nr_symbols)]
+
+    password_list = password_letters+password_numbers+password_symbols
+    random.shuffle(password_list)
+    password="".join(password_list)
+    password_input.insert(0,password)
+    pyperclip.copy(password)
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -26,15 +67,16 @@ website_input.focus()
 
 user_input = Entry(width=35)
 user_input.grid(row=2, column=1, columnspan=2, sticky="w")
+user_input.insert(0, "dummy@gmail.com")
 
 password_input = Entry(width=35)
 password_input.grid(row=3, column=1, sticky="w")
 
 # Buttons
-generate_password_button = Button(text="Generate Password", command="test", padx=0, pady=0,width=15)
+generate_password_button = Button(text="Generate Password", command=password_generator, padx=0, pady=0,width=15)
 generate_password_button.grid(row=3, column=2, sticky="w", padx=5)
 
-add_button = Button(text="Add", command="test", width=15)
+add_button = Button(text="Add", command=save, width=15)
 add_button.grid(row=4, column=1)
 
 window.mainloop()
