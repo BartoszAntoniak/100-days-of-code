afdff60e1315cc53f888ea4626d43a465a0c069e
Bartosz Antoniak <bartosz.antoniak1@gmail.com>
2024-12-18 20:07:52 +0100

    Day31 Language learning flash card project - card flip from french to english after 3 seconds added - saving data about what user already knows added

--- day-31 Flash Card App/main.py
+++ day-31 Flash Card App/main.py
@@ -1,33 +1,56 @@
-from random import randint
+import random
 from tkinter import *
 from tkinter import Canvas
 import pandas
 
+
 BACKGROUND_COLOR = "#B1DDC6"
-#_______________________Timer______________________________
-
-
 #_______________________Data_______________________________
-data = pandas.read_csv("./data/french_words.csv")
-to_learn = data.to_dict()
-key_count=0
-for each_key in to_learn["French"]:
-    key_count+=1
+try:
+    data = pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("./data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+current_card = {}
+flip_timer = None
 
 def next_card():
-    canvas.itemconfig(title_label,text="French")
-    canvas.itemconfig(word_label, text=to_learn["French"][randint(0,key_count-1)])
+    global current_card, flip_timer
+    if flip_timer:
+        window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(title_label, text="French")
+    canvas.itemconfig(word_label, text=current_card["French"])
+    canvas.itemconfig(canvas_image, image=card_front_png)
+    flip_timer = window.after(3000, func=flip_card)
 
+def flip_card():
+    canvas.itemconfig(title_label,text="English")
+    canvas.itemconfig(word_label,text=current_card["English"])
+    canvas.itemconfig(canvas_image, image=card_back_png)
+
+
+def user_knew_card():
+    if current_card == {}:
+        next_card()
+    else:
+        to_learn.remove(current_card)
+        data = pandas.DataFrame(to_learn)
+        data.to_csv("./data/words_to_learn.csv",index=False)
+        next_card()
 #________________________UI________________________________
 #Background
 window = Tk()
 window.title("Flash Card App")
 window.config(background=BACKGROUND_COLOR,padx=50,pady=50)
 
-
 canvas = Canvas(width=800, height=526)
 card_front_png = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400,263,image=card_front_png)
+card_back_png = PhotoImage(file="./images/card_back.png")
+# canvas.create_image(400,263,image=card_front_png)
+canvas_image = canvas.create_image(400, 263, image=card_front_png)
 title_label = canvas.create_text(400, 150,text="Title",font=("Arial",40,"italic"))
 word_label = canvas.create_text(400, 263,text="Word",font=("Arial",60,"bold"))
 canvas.create_text(400,150)
@@ -36,7 +59,7 @@ canvas.grid(row=0, column=0,columnspan=2)
 
 #Buttons
 ok_button_image = PhotoImage(file="./images/right.png")
-ok_button = Button(image=ok_button_image, command=next_card, highlightthickness=0)
+ok_button = Button(image=ok_button_image, command=user_knew_card, highlightthickness=0)
 ok_button.grid(row=1, column=1)
 
 nok_button_image = PhotoImage(file="./images/wrong.png")
